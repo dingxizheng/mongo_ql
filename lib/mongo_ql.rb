2582bbf16ff0112841f78ea120d620a8a3c713db
dingxizheng <dingxizheng@gmail.com>
2019-10-17 12:38:28 -0400

    define deep_transform_values if not defined in activesupport

--- lib/mongo_ql.rb
+++ lib/mongo_ql.rb
@@ -3,6 +3,20 @@
 require "active_support"
 require "active_support/core_ext/hash"
 require "logger"
+
+
+unless {}.respond_to? :deep_transform_values
+  class Hash
+    class Hash
+      def deep_transform_values(&block)
+        reduce({}) do |result, (k, v)|
+          result[k] = v.is_a?(Hash) ? v.deep_transform_values(&block) : yield(v)
+          result
+        end
+      end
+    end
+  end
+end
 module MongoQL
   class InvalidVariableAccess < StandardError; end
 
